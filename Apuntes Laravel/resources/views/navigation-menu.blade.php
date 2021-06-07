61c7365d2e7fc6d24c45e53fd3c3731e3de2d2b7
frotx <jsalmoralp@gmail.com>
2021-06-07 15:35:35 +0200

    Añadiendo nuevos favicon y cambiando links en "tarjetaPresentacio"n

--- Apuntes Laravel/resources/views/navigation-menu.blade.php
+++ Apuntes Laravel/resources/views/navigation-menu.blade.php
@@ -5,26 +5,7 @@
             <div class="absolute inset-y-0 left-0 flex items-center sm:hidden">
                 <button x-on:click="open = true" type="button" class="inline-flex items-center justify-center p-2 text-gray-400 rounded-md hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white" aria-controls="mobile-menu" aria-expanded="false">
                     <span class="sr-only">Open main menu</span>
-                    <!--
-                        Icon when menu is closed.
-
-                        Heroicon name: outline/menu
-
-                        Menu open: "hidden", Menu closed: "block"
-                    -->
-                    <svg class="block w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M4 6h16M4 12h16M4 18h16" />
-                    </svg>
-                    <!--
-                        Icon when menu is open.
-
-                        Heroicon name: outline/x
-
-                        Menu open: "block", Menu closed: "hidden"
-                    -->
-                    <svg class="hidden w-6 h-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor" aria-hidden="true">
-                        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
-                    </svg>
+                    <img class="block md::hidden w-auto h-8" src="logos/eat-sleep-code.png" alt="Eat Sleep Code">
                 </button>
             </div>
 
@@ -32,8 +13,7 @@
             <div class="flex items-center justify-center flex-1 sm:items-stretch sm:justify-start">
                 {{-- logotipo --}}
                 <a href="/" class="flex items-center flex-shrink-0">
-                    <img class="block w-auto h-8 lg:hidden" src="https://tailwindui.com/img/logos/workflow-mark-indigo-500.svg" alt="Workflow">
-                    <img class="hidden w-auto h-8 lg:block" src="https://tailwindui.com/img/logos/workflow-logo-indigo-500-mark-white-text.svg" alt="Workflow">
+                    <img class="hidden w-auto h-8 md:block" src="logos/eat-sleep-code.png" alt="Eat Sleep Code">
                 </a>
 
                 {{-- menu lg --}}
@@ -48,7 +28,7 @@
 
                         <a href="#" class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Calendar</a> --}}
 
-                        <div x-data="{ open: false }" class="inline-block"> {{-- TODO: Implementar para vista mobile --}}
+                        <div x-data="{ open: false }" class="inline-block">
                             <button x-on:click="open = true" type="button" class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white" id="main-menu-button" aria-expanded="false" aria-haspopup="true">
                                 <span class="sr-only">Abrir menu Main</span>
                                 <span>Main</span>
@@ -141,13 +121,26 @@
 <div class="sm:hidden" id="mobile-menu" x-show="open" x-on:click.away="open = false">
     <div class="px-2 pt-2 pb-3 space-y-1">
         <!-- Current: "bg-gray-900 text-white", Default: "text-gray-300 hover:bg-gray-700 hover:text-white" -->
-        <a href="#" class="block px-3 py-2 text-base font-medium text-white bg-gray-900 rounded-md" aria-current="page">Dashboard</a>
+        <div x-data="{ open: false }" class="inline-block">
+            <button x-on:click="open = true" type="button" class="px-3 py-2 text-sm font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white" id="main-menu-button" aria-expanded="false" aria-haspopup="true">
+                <span class="sr-only">Abrir menu Main</span>
+                <span>Main</span>
+            </button>
+            <div x-show="open" x-on:click.away="open = false" class="absolute w-48 py-1 mt-2 origin-top-left bg-white rounded-md shadow-lg left0-0 ring-1 ring-black ring-opacity-5 focus:outline-none" role="menu" aria-orientation="vertical" aria-labelledby="user-menu-button" tabindex="-1">
+                <!-- Active: "bg-gray-100", Not Active: "" -->
+                <a href="{{ route('tarjetaPresentacion') }}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">
+                    Joan Salmoral Parramon
+                </a>
+                <a href="{{ route('homeLaravel') }}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">
+                    Home Laravel
+                </a>
+                <a href="{{ route('homeJetstream') }}" class="block px-4 py-2 text-sm text-gray-700" role="menuitem" tabindex="-1" id="user-menu-item-0">
+                    Home Jetstream
+                </a>
+            </div>
+        </div>
 
-        <a href="#" class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Team</a>
-
-        <a href="#" class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Projects</a>
-
-        <a href="#" class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Calendar</a>
+        <a href="{{route('blog.index')}}" class="block px-3 py-2 text-base font-medium text-gray-300 rounded-md hover:bg-gray-700 hover:text-white">Blog</a>
     </div>
 </div>
 </nav>
--- Apuntes Laravel/resources/views/personal/tarjetaPresentacion.blade.php
+++ Apuntes Laravel/resources/views/personal/tarjetaPresentacion.blade.php
@@ -1,19 +1,19 @@
 <x-app-layout>
     <div class="max-w-5xl px-2 py-8 mx-auto sm:px-6 lg:px-8">
-        <div class="w-full bg-blue-300 bg-center bg-cover rounded-lg">
+        <div class="w-full bg-blue-300 bg-center bg-cover rounded-lg p-5">
             <h1 class="text-5xl font-bold text-center">{{__('Hello!! I am')}} Joan Salmoral Parramon</h1>
             <p class="pt-8 text-xl">Esta web es de <b>demostración</b>, y para que las empresas vean <b>mis habilidades con la programación</b>.</p>
             <p class="pt-2 text-base">Puedes crear un usuario normal, o puedes ver todo lo que ofrece mi web (incluyendo el panel de administrador completo) usando mis credenciales.
             </p>
             <ul class="list-disc list-inside">
-                <li class="text-base list-item">User: jsalmoralp@gmail.com</li>
-                <li class="text-base list-item">Password → contraseña</li>
+                <li class="text-base list-item">User:  jsalmoralp@gmail.com</li>
+                <li class="text-base list-item">Password:  contraseña</li>
             </ul>
             <p class="pt-2 text-lg"><span class="text-green-500 underline">Siéntete libre de hacer cualquier modificación desde el panel de control</span> ya que el contenido que se puede modificar desde los paneles de administración (ya sea de la web o del blog), esta auto generado con los Seeders de Laravel.</p>
             <p class="pt-6 text-3xl text-center">¡¡Ale pues a navegar y echale un buen vistazo a todas las opciones y características!!</p>
         </div>
 
-        <div class="grid grid-cols-1 gap-6 mt-5 md:grid-cols-2 lg:grid-cols-3">
+        <section class="grid grid-cols-1 gap-6 mt-5 md:grid-cols-2 lg:grid-cols-3">
             <article class="w-full bg-center bg-cover rounded-lg h-80 md:col-span-2" style="background-image: url('/images/JoanSalmoralParramon.png')">
                 <div class="flex flex-col justify-center w-full h-full px-8">
                     <div>
@@ -21,10 +21,10 @@
                     </div>
                     <h2 class="mt-2 text-4xl font-bold leading-8 text-purple-500">
                         {{__('My networks')}}:
-                        <a href="https://www.linkedin.com/in/jsalmoralp/" class="inline-block">
+                        <a target="_blank" href="https://www.linkedin.com/in/jsalmoralp/" class="inline-block">
                             <img class="object-cover object-center w-10 h-10" src="/images/linkedin.png" alt="LinkedIn Logo">
                         </a>
-                        <a href="https://github.com/jsalmoralp" class="inline-block">
+                        <a target="_blank" href="https://github.com/jsalmoralp" class="inline-block">
                             <img class="object-cover object-center w-10 h-10" src="/images/github.png" alt="GitHub Logo">
                         </a>
                     </h2>
@@ -36,7 +36,8 @@
                         <span class="inline-block px-3 text-black bg-purple-300 rounded-full h6">PHP</span>
                     </div>
                     <h2 class="mt-2 text-4xl font-bold leading-8 text-white">
-                        <a href="https://github.com/jsalmoralp/PHP-Laravel-Apuntes">{{__('Notes')}} Laravel</a>
+                        <a target="_blank" href="https://github.com/jsalmoralp/PHP-Laravel-Apuntes">Laravel</a>
+                        <br><small>{{__('Notes')}}</small>
                     </h2>
                 </div>
             </article>
@@ -46,7 +47,8 @@
                         <span class="inline-block px-3 text-black bg-purple-300 rounded-full h6">Kotlin</span>
                     </div>
                     <h2 class="mt-2 text-4xl font-bold leading-8 text-white">
-                        <a href="https://github.com/jsalmoralp/AndroidStudio-Apuntes">{{__('Notes')}} AndroidStudio</a>
+                        <a target="_blank" href="https://github.com/jsalmoralp/Kotlin-AndroidStudio-Apuntes">AndroidStudio</a>
+                        <br><small>{{__('Notes')}}</small>
                     </h2>
                 </div>
             </article>
@@ -56,7 +58,20 @@
                         <span class="inline-block px-3 text-black bg-purple-300 rounded-full h6">PHP</span>
                     </div>
                     <h2 class="mt-2 text-4xl font-bold leading-8 text-white">
-                        <a href="https://github.com/jsalmoralp/PHP-Librerias">{{__('Library')}} PHP</a>
+                        <a target="_blank" href="https://github.com/jsalmoralp/PHP-Librerias">PHP</a>
+                        <br><small>{{__('Library')}}</small>
+                    </h2>
+                </div>
+            </article>
+            <article class="w-full bg-blue-500 bg-center bg-cover rounded-lg h-80">
+                <div class="flex flex-col justify-center w-full h-full px-8">
+                    <div>
+                        <span class="inline-block px-3 text-black bg-purple-300 rounded-full h6">PHP</span>
+                        <span class="inline-block px-3 text-black bg-purple-300 rounded-full h6">Json</span>
+                    </div>
+                    <h2 class="mt-2 text-4xl font-bold leading-8 text-white">
+                        <a target="_blank" href="https://github.com/jsalmoralp/PHP-Composer-RiotApi_LeagueOfLegends">PHP RiotAPI</a>
+                        <br><small>Lector web Json</small>
                     </h2>
                 </div>
             </article>
@@ -66,7 +81,8 @@
                         <span class="inline-block px-3 text-black bg-purple-300 rounded-full h6">JavaScript</span>
                     </div>
                     <h2 class="mt-2 text-4xl font-bold leading-8 text-white">
-                        <a href="https://github.com/jsalmoralp/Discord_Bot-General-GeneralCow-GeneralSomethings">DiscordBot "General Cow"</a>
+                        <a target="_blank" href="https://github.com/jsalmoralp/JavaScript-DiscordBot-General">DiscordBot</a>
+                        <br><small>General Cow</small>
                     </h2>
                 </div>
             </article>
@@ -76,10 +92,22 @@
                         <span class="inline-block px-3 text-black bg-purple-300 rounded-full h6">Java</span>
                     </div>
                     <h2 class="mt-2 text-4xl font-bold leading-8 text-white">
-                        <a href="https://github.com/jsalmoralp/Plugin_Minecraft-All_in_One">MinecraftPlugin "AllInOne"</a>
+                        <a target="_blank" href="https://github.com/jsalmoralp/Java-PluginMinecraft-AllInOne">Plugin Minecraft</a>
+                        <br><small>All In One</small>
                     </h2>
                 </div>
             </article>
-        </div>
+            <article class="w-full bg-gad bg-yellow-300 bg-cover rounded-lg h-80">
+                <div class="flex flex-col justify-center w-full h-full px-8">
+                    <div>
+                        <span class="inline-block px-3 text-black bg-purple-300 rounded-full h6">Python</span>
+                    </div>
+                    <h2 class="mt-2 text-4xl font-bold leading-8 text-white">
+                        <a target="_blank" href="https://github.com/jsalmoralp/Python-DiscordBot-Apuntes">Discord Bot</a>
+                        <br><span>Apuntes</span>
+                    </h2>
+                </div>
+            </article>
+        </section>
     </div>
 </x-app-layout>
